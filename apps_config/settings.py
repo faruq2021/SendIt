97991dad46b43a6f4670e3469afed57035fe355a
faruq2021 <umardauda@rocketmail.com>
2022-03-11 13:55:34 +0100

    Update settings.py

--- apps_config/settings.py
+++ apps_config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from django import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,3 +136,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+django_heroku.settings(locals())
